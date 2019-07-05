b2ca2cd15ec6ae274b3a580be3fdba7404151d69
Xelanor <berozel@hotmail.com>
2019-07-05 19:46:19 +0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Navbar from "./components/Navbar/Navbar"
+import Homepage from './components/HomePage/Homepage'
+import Auth from './containers/Auth/Auth'
+
+class App extends Component {
+  state = {}
+  render() {
+    return (
+      <Router>
+        <div>
+          <Navbar />
+          <br />
+          <Route path="/" exact component={Homepage} />
+          <Route path="/auth" component={Auth} />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
